9e1a7b56f8661aa34465375941086d4503a17e11
HidalgoJTD4C <hidalgojt4@gmail.com>
2025-03-05 19:24:14 -0400

    formulario
    formulario de registro cargado

--- sada-inac/src/app/pages/registro/registro.component.ts
+++ sada-inac/src/app/pages/registro/registro.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
+import { RegisterComponent } from '../../components/register/register.component';
 
 @Component({
   selector: 'app-registro',
-  imports:[HeaderComponent],
+  imports:[HeaderComponent, RegisterComponent],
   templateUrl: './registro.component.html',
   styleUrl: './registro.component.css'
 })
