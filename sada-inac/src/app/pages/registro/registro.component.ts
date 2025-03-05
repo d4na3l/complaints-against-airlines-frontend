ee61812a0b9ff2adb2a3229244e908fe7df67d11
HidalgoJTD4C <hidalgojt4@gmail.com>
2025-03-05 19:30:59 -0400

    formulario

--- sada-inac/src/app/pages/registro/registro.component.ts
+++ sada-inac/src/app/pages/registro/registro.component.ts
@@ -1,17 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
-<<<<<<< HEAD
 import { RegisterComponent } from '../../components/register/register.component';
 
 @Component({
   selector: 'app-registro',
   imports:[HeaderComponent, RegisterComponent],
-=======
-
-@Component({
-  selector: 'app-registro',
-  imports:[HeaderComponent],
->>>>>>> origin/josuha
   templateUrl: './registro.component.html',
   styleUrl: './registro.component.css'
 })
